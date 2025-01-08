9d955b8ba9d97d4b19439636516e899766bb81b9
Vanshika <vanshikavishal354@gmail.com>
2025-01-08 16:06:31 +0530

    Update game.js

--- game.js
+++ game.js
@@ -125,7 +125,8 @@ function addIconToContainer() {
     iconsContainer.innerHTML = ''; 
         iconList.forEach(iconName => {
             const img = document.createElement('img');
-            img.src = `icons/${iconName}.png`; 
+            // img.src = `icons/${iconName}.png`; 
+            img.src = icons_map[iconName];
             img.alt = iconName; 
             img.title = iconName; 
             img.style.width = '50px'; 
@@ -281,13 +282,14 @@ gridOverlay.addEventListener('click', (e) => {
     const text = gridTextList[gridNumber] || '';
     inputText = text;
     loadFloatingImage(inputText);
+    
 });
 
 function loadFloatingImage(inputText) {
     console.log('Loading image for:', inputText);
     const image = document.getElementById('floatingImage');
     const errorMessage = document.getElementById('errorMessage');
-    const imageUrl = "imagSes/" + inputText + ".png"; 
+    const imageUrl = image_map[inputText]; 
    
     errorMessage.style.display = 'none';
     
@@ -413,5 +415,118 @@ window.addEventListener('click', (event) => {
     }
 });
 
+const image_map = {
+    "Academia": "https://i.ibb.co/ypP9RXh/academia.png",
+    "Accelerator": "https://i.ibb.co/br5TwQd/Accelerator.png",
+    "Acquisition": "https://i.ibb.co/T4vmGrq/acquisition.png",
+    "Ananya": "https://i.ibb.co/9WdQG4t/ananya.png",
+    "Angel funding": "https://i.ibb.co/XYsj1Mv/Angel-funding.png",
+    "App development": "https://i.ibb.co/W5qKg2Y/App-development.png",
+    "Bank": "https://i.ibb.co/SVZXRXn/Bank.png",
+    "Bg": "https://i.ibb.co/HpJ8K2B/bg.jpg",
+    "Big Corp": "https://i.ibb.co/vVsFh6v/Big-Corp.png",
+    "Buy out": "https://i.ibb.co/XV0ZnTS/Buy-out.png",
+    "Comp Reg": "https://i.ibb.co/2vkDfmn/comp-reg.png",
+    "Company Registration": "https://i.ibb.co/WkhQ8mH/Company-Registration.png",
+    "CRM": "https://i.ibb.co/jTzcH5c/crm.png",
+    "Crowd funding": "https://i.ibb.co/jM9Bm2k/Crowd-funding.png",
+    "Customer Persona": "https://i.ibb.co/nBnR0Bw/customer-persona.jpg",
+    "Customers": "https://i.ibb.co/XSCT5F1/customers.png",
+    "EBG Board": "https://i.ibb.co/Cz4MvRq/EBG-board.jpg",
+    "EBG Box": "https://i.ibb.co/nLq4Qdf/EBG-box.jpg",
+    "Ebg Logo": "https://i.ibb.co/z6DmDHT/ebg-logo.png",
+    "EBG V7": "https://i.ibb.co/DWyHbW7/EBG-v7.jpg",
+    "EBG V8": "https://i.ibb.co/QYR3GgS/EBG-v8.jpg",
+    "Enabler": "https://i.ibb.co/HNVMK7D/Enabler.png",
+    "Equipment": "https://i.ibb.co/SB3bKnv/equipment.png",
+    "FFF funding": "https://i.ibb.co/C6Zrv5P/fff-funding.png",
+    "Goal": "https://i.ibb.co/h12DrL1/goal.png",
+    "Grant": "https://i.ibb.co/PrJbPYd/Grant.png",
+    "Hackathon": "https://i.ibb.co/16Yk5Ny/hackathon.png",
+    "Harry": "https://i.ibb.co/GFcnYgr/harry.png",
+    "Idea": "https://i.ibb.co/tXG8b31/Idea.png",
+    "Ideathon": "https://i.ibb.co/xFDBn5d/Ideathon.png",
+    "Incubator": "https://i.ibb.co/s9StkCD/incubator.png",
+    "Indresh": "https://i.ibb.co/qjxqXzH/indresh.png",
+    "Insurance": "https://i.ibb.co/B6H1YRw/insurance.png",
+    "Internal Partnership": "https://i.ibb.co/MZZWPTk/Internal-Partnership.png",
+    "International reach": "https://i.ibb.co/B4krQP1/International-reach.png",
+    "IP rights": "https://i.ibb.co/5WY4N8y/IP-rights.png",
+    "IT Setup": "https://i.ibb.co/vJbDFgb/IT-setup.png",
+    "Laksh": "https://i.ibb.co/GJtVv2v/laksh.png",
+    "Market Survey": "https://i.ibb.co/WBrQ1fc/Market-Survey.png",
+    "Marketing": "https://i.ibb.co/C8htD3h/Marketing.png",
+    "Mentor": "https://i.ibb.co/DKSz3Nh/Mentor.png",
+    "MVP": "https://i.ibb.co/310288r/MVP.png",
+    "Office": "https://i.ibb.co/P5frbbT/office.png",
+    "Pivot": "https://i.ibb.co/DRPXRfH/Pivot.png",
+    "Prototype": "https://i.ibb.co/166sswg/prototype.png",
+    "Revenue": "https://i.ibb.co/yRdnWZC/revenue.png",
+    "Seed funding": "https://i.ibb.co/5vkGN1J/Seed-funding.png",
+    "Skill Dev": "https://i.ibb.co/ZdKW3bT/skill-dev.png",
+    "Skill Development": "https://i.ibb.co/8dwtKjX/skill-development.png",
+    "Sunny": "https://i.ibb.co/kc1C780/sunny.png",
+    "Tax": "https://i.ibb.co/6XqLNjt/Tax.png",
+    "Team": "https://i.ibb.co/ByyLMCD/team.png",
+    "VC funding": "https://i.ibb.co/Sny1hDK/VC-Funding.png",
+    "Vinny": "https://i.ibb.co/MfZkZfj/vinny.png"
+}
 
-
+icons_map = {
+    "Academia": "https://i.ibb.co/rsNWbkX/Academia.png",
+    "Accelerator": "https://i.ibb.co/KVQbNGT/Accelerator.png",
+    "Acquisition": "https://i.ibb.co/8rmyQkG/Acquisition.png",
+    "Ananya": "https://i.ibb.co/fvKvG9W/ananya.png",
+    "Angel funding": "https://i.ibb.co/XZDrsjT/Angel-Funding.png",
+    "Bank": "https://i.ibb.co/FntMZJ2/Bank.png",
+    "Big corp": "https://i.ibb.co/BL4C6TC/Big-Corp.png",
+    "Buy out": "https://i.ibb.co/6Wp4nwp/Buy-Out.png",
+    "Company registration": "https://i.ibb.co/M7XDDBg/Company-Registration.png",
+    "CRM": "https://i.ibb.co/vdQvDBd/CRM.png",
+    "Crowd funding": "https://i.ibb.co/3YG2MCW/Crowd-Funding.png",
+    "Customer 1k": "https://i.ibb.co/GdzPznm/Customer-1k.png",
+    "Customer 10k": "https://i.ibb.co/hVtbd2L/Customer-10k.png",
+    "Customer 100 K": "https://i.ibb.co/dbk8Dgr/Customer-100-K.png",
+    "Customer multiplier": "https://i.ibb.co/p4MPsNs/Customer-Multiplier.png",
+    "Customers": "https://i.ibb.co/MhWWLVH/Customers.png",
+    "Dice": "https://i.ibb.co/ZxQms7h/Dice.png",
+    "Enabler": "https://i.ibb.co/0sqrHmN/Enabler.png",
+    "Equipment": "https://i.ibb.co/ss4WzMS/Equipment.png",
+    "Equity": "https://i.ibb.co/K9YdjHf/Equity.png",
+    "Factory": "https://i.ibb.co/h72Bnfk/factory.png",
+    "FFF funding": "https://i.ibb.co/5rRxYF4/FFF-funding.png",
+    "Grant": "https://i.ibb.co/p2cvCj7/Grant.png",
+    "Hackathon": "https://i.ibb.co/HCTC6M5/Hackathon.png",
+    "Harry": "https://i.ibb.co/jfc3WxQ/harry.png",
+    "Idea": "https://i.ibb.co/hYybM8f/Idea.png",
+    "Ideathon": "https://i.ibb.co/1vM5N1x/ideathon.png",
+    "Incubator": "https://i.ibb.co/xHhMqYf/Incubator.png",
+    "Indresh": "https://i.ibb.co/9cpW0Px/indresh.png",
+    "Insurance": "https://i.ibb.co/7SHw6W3/insurance.png",
+    "Internal partnership": "https://i.ibb.co/G7xtGd4/internal-partnership.png",
+    "International reach": "https://i.ibb.co/XFYgmgF/International-reach.png",
+    "IP rights": "https://i.ibb.co/C6ZJBxV/IP-rights.png",
+    "IT setup": "https://i.ibb.co/qjcjztN/IT-setup.png",
+    "Laksh": "https://i.ibb.co/JQ1LySH/Laksh.png",
+    "Market survey": "https://i.ibb.co/QQf4RhN/Market-Survey.png",
+    "Marketing": "https://i.ibb.co/2YPny1M/Marketing.png",
+    "Mentor": "https://i.ibb.co/pfHTpfV/Mentor.png",
+    "MVP V1": "https://i.ibb.co/wc8XTgV/MVP-V1.png",
+    "MVP V2": "https://i.ibb.co/YLx6bgz/MVP-V2.png",
+    "MVP V3": "https://i.ibb.co/8jYtkQw/MVP-V3.png",
+    "MVP V4": "https://i.ibb.co/9t9DDrH/MVP-V4.png",
+    "MVP": "https://i.ibb.co/ZR9q48Z/MVP.png",
+    "Net worth": "https://i.ibb.co/brDkh2m/Net-worth.png",
+    "Office": "https://i.ibb.co/p4nKV7J/office.png",
+    "Pivot": "https://i.ibb.co/kHCDPGX/Pivot.png",
+    "Prototype": "https://i.ibb.co/jW8PfkR/Prototype.png",
+    "Revenue multiplier": "https://i.ibb.co/99dQ2DF/Revenue-Multiplier.png",
+    "Revenue": "https://i.ibb.co/p1w8tt9/Revenue.png",
+    "Seed funding": "https://i.ibb.co/nwhr2Rj/Seed-funding.png",
+    "Skill development": "https://i.ibb.co/vj1W0Wg/skill-development.png",
+    "Sunny": "https://i.ibb.co/vJYQxCd/sunny.png",
+    "Tax": "https://i.ibb.co/cF4vQkT/Tax.png",
+    "Team": "https://i.ibb.co/18SyZ8H/Team.png",
+    "VC funding": "https://i.ibb.co/p67B3wS/VC-Funding.png",
+    "Vinny": "https://i.ibb.co/PrJ9D0f/vinny.png"
+}
